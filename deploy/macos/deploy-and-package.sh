bb72e3498e5cb65496ad63224eb7817a67fc0201
Chris Cannam <cannam@all-day-breakfast.com>
2023-09-19 18:39:50 +0100

    Further package fixes

--- deploy/macos/deploy-and-package.sh
+++ deploy/macos/deploy-and-package.sh
@@ -61,9 +61,7 @@ mkdir "$volume" || exit 1
 
 ln -s /Applications "$volume"/Applications
 cp README.md "$volume/README.txt"
-cp README_OSC.md "$volume/README_OSC.txt"
 cp COPYING "$volume/COPYING.txt"
-cp CHANGELOG "$volume/CHANGELOG.txt"
 cp CITATION "$volume/CITATION.txt"
 
 for builddir in $builddirs; do
