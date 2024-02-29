cd8e44924ac9b55f16f887bd4ffbc7677b917732
Chris Cannam <cannam@all-day-breakfast.com>
2024-02-29 14:58:47 +0000

    Notarize fix

--- deploy/macos/notarize.sh
+++ deploy/macos/notarize.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-## The following assumes we have generated an app password at
-## appleid.apple.com and then stored it to keychain id "altool" using
-## e.g.
-## security add-generic-password -a "cannam+apple@all-day-breakfast.com" \
-##   -w "generated-app-password" -s "altool"
-
 ## NB to verify:
 # spctl -a -v "/Applications/Application.app"
 
@@ -26,7 +20,7 @@ xcrun notarytool submit \
     "$dmg" \
     --apple-id "$user" \
     --team-id "$team_id" \
-    --keychain-profile notarytool \
+    --keychain-profile notarytool-cannam \
     --wait --progress
 
 echo
